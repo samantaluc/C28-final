ad265d1e3cd69f8fc8722af641b67c705e6b0cdb
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-17 13:20:40 +0530

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -1,25 +1,15 @@
 class CannonBall {
   constructor(x, y) {
     var options = {
-      restitution: 0.8,
-     // friction: 1.0,
-      //density: 1.0,
+     //add resistution 
       isStatic: true
     };
-    this.r = 40;
-
-    this.body = Bodies.circle(x, y, this.r, options);
-
-    this.image = loadImage("./assets/cannonball.png");
+//add circle to the body
+//loadImage
     World.add(world, this.body);
   }
 
-  shoot() {
-    var velocity = p5.Vector.fromAngle(cannon.angle);
-    velocity.mult(20);
-    Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
-  }
+  //add shoot function 
 
   display() {
     var angle = this.body.angle;
@@ -28,7 +18,7 @@ class CannonBall {
     translate(pos.x, pos.y);
     rotate(angle);
     imageMode(CENTER);
-    image(this.image, 0, 0, this.r, this.r);
+//call the image function
     pop();
 
     }
